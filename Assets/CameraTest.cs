ec7a674bce90cb0c1f4dd3ae2d60548043c4d69a
David <david.j.steiger@web.de>
2018-10-23 14:34:53 +0200

    Erster test
    Basic functions ausprobiert.

---
+++ Assets/CameraTest.cs
@@ -0,0 +1,20 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTest : MonoBehaviour {
+
+    public Rigidbody hoehe;
+    public Transform rotate;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Camera:Script \n Ich funktioniere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hoehe.AddForce(0, 10, 0 * Time.deltaTime);
+        //rotate.Rotate(1,0,0 * Time.deltaTime);
+	}
+}
