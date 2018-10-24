aec93660119b19ff4a8020b4c583984993ebc2a2
David <david.j.steiger@web.de>
2018-10-24 15:02:18 +0200

    korridor

---
+++ Assets/CameraTest.cs
@@ -0,0 +1,22 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTest : MonoBehaviour {
+
+    public Rigidbody hoehe;
+    public Transform rotate;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Camera-Script: \n Ich funktioniere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hoehe.AddForce(0, 5, 0 * Time.deltaTime);
+        //if(rotate.rotation.x<=30){
+            rotate.Rotate((float)0.2, 0, 0 * Time.deltaTime);
+        //}
+	}
+}
