567767dc053d6b05b7dce772611d41f6c5070840
David <david.j.steiger@web.de>
2018-11-06 08:50:50 +0100

    2018_11_06_David

--- Assets/Scripts/CameraTest.cs
+++ Assets/Scripts/CameraTest.cs
@@ -14,14 +14,18 @@ public class CameraTest : MonoBehaviour {
     // Use this for initialization
     void Start () {
     
+        /*
         hoehe.AddForce(Force * Time.deltaTime, 0, 0);
+
         currentX = 0;
         currentY = 90;
+        */
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
     
+       /*
         //Rotation nach rechts per Rechtspfeil;
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -46,12 +50,19 @@ public class CameraTest : MonoBehaviour {
             Camera.main.transform.rotation = Quaternion.Euler(currentX, currentY, 0);
         }
 
-        ////Rotation nach unten per Pfeil;
+        //Rotation nach unten per Pfeil;
         if (Input.GetKey(KeyCode.DownArrow))
         {
             currentX = rotate.eulerAngles.x;
             currentX += 2;
             Camera.main.transform.rotation = Quaternion.Euler(currentX, currentY, 0);
         }
+
+        //Wenn "s" gedrückt wird, wird Kraft nach hinten auf Kamera addiert
+        if (Input.GetKey("s")){
+                hoehe.AddForce(negForce * Time.deltaTime, 0, 0);
+        }
+    */
     }
+
 }
