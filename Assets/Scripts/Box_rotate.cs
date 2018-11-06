096d41f10d157b3e8d5fc3dd9e6fce3a8e434664
David <david.j.steiger@web.de>
2018-11-06 16:27:06 +0100

    Google_VR_Assets

---
+++ Assets/Scripts/Box_rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_rotate : MonoBehaviour {
+
+
+    public float spin;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, spin * Time.deltaTime, 0);
+	}
+
+    public void spinChange(){
+        spin = -spin;
+    }
+}
