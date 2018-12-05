121170f67f33c662f5a5f6aa9c0344a14db22ff1
David <david.j.steiger@web.de>
2018-12-05 10:46:15 +0100

    2018_12_05_v02_David

---
+++ Assets/Scripts/Leuchtstab_Go.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leuchtstab_Go : MonoBehaviour {
+
+    public Rigidbody rightStaff, middleStaff, leftStaff;
+    public int yVelo1, yVelo2, yVelo3;
+
+    // Use this for initialization
+	void Start () {
+        //myStaff.AddForce(0, yVelo * Time.deltaTime, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //myStaff.AddForce(0, yVelo * Time.deltaTime, 0);
+    }
+
+    public void goUp(){
+        rightStaff.AddForce(0, yVelo3 * Time.deltaTime, 0);
+    }
+
+    public void goDown(){
+        leftStaff.AddForce(0, yVelo1 * Time.deltaTime, 0);
+    }
+}
